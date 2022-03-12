66daddc04fe816d8146413f6522a7a0389cba33c
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-03-12 14:36:05 +0100

    del 4

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Product } from './Product'
-
-import {products} from './Data/ProductData'
+import {useState,useEffect} from 'react'
+import {fetchProducts} from './Data/ProductData'
 
 export const Products = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() =>{
+    const fetchData = async()=>{
+      const result = await fetchProducts()
+      setProducts(result)
+    }
+    fetchData()
+  }, [])
+
+
   return (
     <section className="products" id="products">
             <h1 className="heading"> <span>our</span> products </h1>
