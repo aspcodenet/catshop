f04d55dbdcfdf58effa98e7d3065ed4f7a9583ba
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-03-12 11:31:46 +0100

    del3

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Product } from './Product'
+
+import {products} from './Data/ProductData'
 
 export const Products = () => {
   return (
-    <div>Products</div>
+    <section className="products" id="products">
+            <h1 className="heading"> <span>our</span> products </h1>
+        
+            <div className="productgallery">
+                {
+                    products.map((theProduct,i)=> <Product key={i} product={theProduct} />    )
+                }
+            </div>
+        
+        </section>
   )
 }
